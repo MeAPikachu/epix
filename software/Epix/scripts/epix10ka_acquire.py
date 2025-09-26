fd3c34908ee018b8fc0c6c4c4643161c0ac84448
Chengjie <chengjie@stanford.edu>
2025-09-25 19:58:47 -0700

    debug

--- software/Epix/scripts/epix10ka_acquire.py
+++ software/Epix/scripts/epix10ka_acquire.py
@@ -112,13 +112,6 @@ def main():
     acq_log.setLevel(myloglevel)
     # logging.getLogger('pyrogue').setLevel(logging.DEBUG)
 
-    if args.asic:
-        asic = args.asic[0] % 4
-        acq_log.info("Config ASIC %d" % (asic))
-    else:
-        asic = 0
-        acq_log.info("[DEFAULT] Config ASIC 0")
-
     if args.yml:
         if not os.path.isfile(args.yml[0]):
             acq_log.error("[%s] yml config file is missing!", args.yml[0])
@@ -154,136 +147,30 @@ def main():
     # add devices to board
     board.add(dw)
     board.add(fpga.Epix10ka(name='Epix10ka', offset=0, memBase=srp, enabled=True))
-
+    board.add(pyrogue.RunControl(name = 'runControl', description='Run Controller ePix 10ka'))
     board.start()
 
     # command board to read ePix config
-    board.readConfig(args.yml[0])
-    targetASIC = getattr(board.Epix10ka, 'Epix10kaAsic' + str(asic))
+    board.LoadConfig(args.yml[0])
+    #targetASIC = getattr(board.Epix10ka, 'Epix10kaAsic' + str(asic))
 
-    if args.bandwidth:
-        checkframe = frameit()
-        pyrogue.streamConnect(pgpVc0, checkframe)
-
-    if args.asic:
-        if args.setmatrix:
-            # targetASIC.PrepareMultiConfig()
-            targetASIC._rawWrite(0x00008000*4, 0)
-            # targetASIC.WriteMatrixData.set(args.setmatrix[0])
-            targetASIC._rawWrite(0x00004000*4, args.setmatrix[0])
-            acq_log.info("Set Matrix to %d" % (args.setmatrix[0]))
-
-        # targetASIC.CmdPrepForRead()
-        targetASIC._rawWrite(0, 0)
-        acq_log.debug("Sent ASIC%d CmdPrepForRead()" % (args.asic[0]))
-    else:
-        if args.setmatrix:
-            mset = args.setmatrix[0] & 0xF  # value is only 4 bits
-            # board.Epix10ka.Epix10kaAsic0.PrepareMultiConfig()
-            board.Epix10ka.Epix10kaAsic0._rawWrite(0x00008000*4, 0)
-            # board.Epix10ka.Epix10kaAsic0.WriteMatrixData.set(args.setmatrix[0])
-            board.Epix10ka.Epix10kaAsic0.WriteMatrixData._rawWrite(0x00004000*4, mset)
-            acq_log.info("A0 Set Matrix to %d" % (args.setmatrix[0]))
-            # board.Epix10ka.Epix10kaAsic1.PrepareMultiConfig()
-            board.Epix10ka.Epix10kaAsic1._rawWrite(0x00008000*4, 0)
-            # board.Epix10ka.Epix10kaAsic1.WriteMatrixData.set(args.setmatrix[0])
-            board.Epix10ka.Epix10kaAsic1.WriteMatrixData._rawWrite(0x00004000*4, mset)
-            acq_log.info("A1 Set Matrix to %d" % (args.setmatrix[0]))
-            # board.Epix10ka.Epix10kaAsic2.PrepareMultiConfig()
-            board.Epix10ka.Epix10kaAsic2._rawWrite(0x00008000*4, 0)
-            # board.Epix10ka.Epix10kaAsic2.WriteMatrixData.set(args.setmatrix[0])
-            board.Epix10ka.Epix10kaAsic2.WriteMatrixData._rawWrite(0x00004000*4, mset)
-            acq_log.info("A2 Set Matrix to %d" % (args.setmatrix[0]))
-            # board.Epix10ka.Epix10kaAsic3.PrepareMultiConfig()
-            board.Epix10ka.Epix10kaAsic3._rawWrite(0x00008000*4, 0)
-            # board.Epix10ka.Epix10kaAsic3.WriteMatrixData.set(args.setmatrix[0])
-            board.Epix10ka.Epix10kaAsic3.WriteMatrixData._rawWrite(0x00004000*4, mset)
-            acq_log.info("A3 Set Matrix to %d" % (args.setmatrix[0]))
-        # board.Epix10ka.Epix10kaAsic0.CmdPrepForRead()
-        # board.Epix10ka.Epix10kaAsic1.CmdPrepForRead()
-        # board.Epix10ka.Epix10kaAsic2.CmdPrepForRead()
-        # board.Epix10ka.Epix10kaAsic3.CmdPrepForRead()
-        board.Epix10ka.Epix10kaAsic0._rawWrite(0, 0)
-        board.Epix10ka.Epix10kaAsic1._rawWrite(0, 0)
-        board.Epix10ka.Epix10kaAsic2._rawWrite(0, 0)
-        board.Epix10ka.Epix10kaAsic3._rawWrite(0, 0)
-        acq_log.debug("Sent ASICs CmdPrepForRead()")
-
-    board.dataWriter.dataFile.set(ofilename)  # tell datawriter where to write data
-
-    # allow user to override tr_bit
-    # this is useful for automating gain modes
-    if args.trbit:
-        ttr = False if args.trbit[0] == 0 else True
-        board.Epix10ka.Epix10kaAsic0.trbit.set(ttr)
-        board.Epix10ka.Epix10kaAsic1.trbit.set(ttr)
-        board.Epix10ka.Epix10kaAsic2.trbit.set(ttr)
-        board.Epix10ka.Epix10kaAsic3.trbit.set(ttr)
-
-    # allow user to override hrtest
-    # this is useful for automating gain modes
-    if args.hrtest:
-        thr = False if args.hrtest[0] == 0 else True
-        board.Epix10ka.Epix10kaAsic0.hrtest.set(thr)
-        board.Epix10ka.Epix10kaAsic1.hrtest.set(thr)
-        board.Epix10ka.Epix10kaAsic2.hrtest.set(thr)
-        board.Epix10ka.Epix10kaAsic3.hrtest.set(thr)
-
-    # enable test bits if we are doing linearity tests
-    if args.linearitytest:
-        board.Epix10ka.Epix10kaAsic0.test.set(True)
-        board.Epix10ka.Epix10kaAsic1.test.set(True)
-        board.Epix10ka.Epix10kaAsic2.test.set(True)
-        board.Epix10ka.Epix10kaAsic3.test.set(True)
-        board.Epix10ka.Epix10kaAsic0.atest.set(True)
-        board.Epix10ka.Epix10kaAsic1.atest.set(True)
-        board.Epix10ka.Epix10kaAsic2.atest.set(True)
-        board.Epix10ka.Epix10kaAsic3.atest.set(True)
-        acq_log.info("Enable Test Bits")
+ 
+    board.dataWriter.DataFile.set(ofilename)  # tell datawriter where to write data
+    board.dataWriter.Open.set(True) 
 
     acq_log.info("Finished configuration. Acquiring data ...")
     # start with a clean slate
     board.Epix10ka.EpixFpgaRegisters.AutoDaqEnable.set(False)
     board.Epix10ka.EpixFpgaRegisters.AutoRunEnable.set(False)
-    board.Epix10ka.EpixFpgaRegisters.AcqCountReset.set(1)
-    board.Epix10ka.EpixFpgaRegisters.AcqCountReset.set(0)
-    board.Epix10ka.EpixFpgaRegisters.SeqCountReset.set(1)
-    board.Epix10ka.EpixFpgaRegisters.SeqCountReset.set(0)
-    if not args.nosave:
-        board.dataWriter.open.set(True)  # open file
-    else:
-        acq_log.info("won't save to disk")
-    # acquire data for specific duration
     board.Epix10ka.EpixFpgaRegisters.AutoDaqEnable.set(True)
     board.Epix10ka.EpixFpgaRegisters.AutoRunEnable.set(True)
-    if args.linearitytest:
-        time.sleep(0.5)
-        board.Epix10ka.Epix10kaAsic0.PulserR.post(True)
-        board.Epix10ka.Epix10kaAsic1.PulserR.post(True)
-        board.Epix10ka.Epix10kaAsic2.PulserR.post(True)
-        board.Epix10ka.Epix10kaAsic3.PulserR.post(True)
-        time.sleep(0.5)
-        board.Epix10ka.Epix10kaAsic0.PulserR.post(False)
-        board.Epix10ka.Epix10kaAsic1.PulserR.post(False)
-        board.Epix10ka.Epix10kaAsic2.PulserR.post(False)
-        board.Epix10ka.Epix10kaAsic3.PulserR.post(False)
-        acq_log.info("Test Pulse Sent")
-
+ 
     time.sleep(args.time[0])  # acquisition time
     acq_log.info("Flushing...")
     board.Epix10ka.EpixFpgaRegisters.AutoDaqEnable.set(False)
     board.Epix10ka.EpixFpgaRegisters.AutoRunEnable.set(False)
-    if not args.nosave:
-        board.dataWriter.open.set(False)
 
-    if args.bandwidth:
-        acq_log.info('Number of lost frames %d out of %d' %
-                     (checkframe.lost, checkframe.processed))
-        acq_log.info('Data Rate %.2f MB/s' %
-                     (checkframe.processed*checkframe.EPIX10KA_FS/args.time[0]/1e6))
-        if checkframe.processed != checkframe.accepted:
-            acq_log.debug('Link Speed %.2f MB/s' %
-                          (checkframe.accepted*checkframe.EPIX10KA_FS/args.time[0]/1e6))
+    board.dataWriter.Open.set(False)
 
     board.stop()
     acq_log.info("Done")
